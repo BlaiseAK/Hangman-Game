206f336174b8b91fa73a65963c5ee2ef34ff9c41
BlaiseAK <blaisekress@yahoo.com>
2018-06-03 21:53:27 -0400

    I've gotten most of the coding complete and it looks like it should run but it doesn't look like it's even taking

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,36 @@
 // create words array
 
-var wordsArr = ["bad", "night", "laugh", "sun"]
+var wordsBankArr = ["tardis", "games", "", "dnd"]
 
-// select word at random from words array
 
-var compChoice = words[Math.floor(Math.random()*words.length)];
 
 // find length of selected word
 
 var wordArr = []
+
+
+
+// establish wins, losses, guesses left, and incorrect letter array
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 12;
+var incorrectArr = [];
+var answerArr = [];
+
+
+
+function newGame() {
+    guessesLeft = 12;
+    incorrectArr = [];
+    answerArr = [];
+    compChoice = wordsBankArr[Math.floor(Math.random()*wordsBankArr.length)];
+
+}
+
+    // select word at random from words array
+
+var compChoice = wordsBankArr[Math.floor(Math.random()*wordsBankArr.length)];
 
 // turn selected word into an array of letters
 
@@ -22,56 +44,63 @@ for (var j = 0; j < compChoice.length; j++); {
     answerArr.push("_");
 }
 
-// establish wins, losses, guesses left, and incorrect letter array
-
-var wins = 0;
-var losses = 0;
-var guessesLeft = 12;
-var incorrectArr = [];
-var answerArr = [];
-var userGuess = event.key.toLowerCase();
+document.onkeyup = function (event) {
 
 
-function newGame() {
-    guessesLeft = 12;
-    incorrectArr = [];
-    answerArr = [];
+    var userGuess = event.key.toLowerCase();
 
-}
-// loop game with condition that selected word array === guessed correct array
+    var indexWordArr = wordArr.indexOf(userGuess)
 
-for (wordArr === answerArr); {
-}
+    var indexIncorrectArr = incorrectArr.indexOf(userGuess)
 
-// if guessed letter is in selected word array then in the guessed correct array replace "_" with letter guessed
 
-var index = wordArr.indexOf(userGuess)
+    // loop game with condition that selected word array === guessed correct array
 
-if (index !== -1) {
-    answerArr[index] = userGuess; 
-}
+    while (wordArr !== answerArr) {
 
-// if guessed letter is not in the selected word array then push guessed letter to incorrect letter array and -1 from guesses left
 
-var indexIncorrect = incorrectArr.indexOf(userGuess)
+        // if guessed letter is in selected word array then in the guessed correct array replace "_" with letter guessed
 
-if ((index === -1) && (indexIncorrect === -1)) {
-    incorrectArr.push(userGuess);
-    guessesLeft --;
-}
+        
 
-// if guesses left = 0 then restart game and add 1 to the losses variable
+        if (indexWordArr !== -1) {
+            answerArr[indexWordArr] = userGuess; 
+        }
 
-if (guessesLeft === 0) {
-    newGame();
-    losses ++;
+        // if guessed letter is not in the selected word array then push guessed letter to incorrect letter array and -1 from guesses left
 
-}
+        
 
-// if word array === guessed correct array then add 1 to the wins variable and restart game
+         else if ((indexWordArr === -1) && (indexIncorrectArr === -1)) {
+            incorrectArr.push(userGuess);
+            guessesLeft --;
+        }
 
-if (wordArr === answerArr) {
-    newGame();
-    wins ++;
+        // if guesses left = 0 then restart game and add 1 to the losses variable   
+
+         else if (guessesLeft === 0) {
+            newGame();
+            losses ++;
+
+        }
+
+        // if word array === guessed correct array then add 1 to the wins variable and restart game
+
+         else if (wordArr === answerArr) {
+            newGame();
+            wins ++;
+
+        }
+    }
+    
+    var html = 
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guessesLeft + "</p>" +
+    "<p>The word: " + wordArr + "</p>" +
+    "<p>The incorrect letters you've guessed</p>" +
+    "<p>" + incorrectArr + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
 
 }
